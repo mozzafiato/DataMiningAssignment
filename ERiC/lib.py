72896eb33b02269026a7b167970e152749e1af37
Melanija Kraljevska <kraljevskamelanija@gmail.com>
2022-11-16 00:46:33 +0100

    sample dataset, bug fixes

--- ERiC/lib.py
+++ ERiC/lib.py
@@ -3,7 +3,7 @@ from sklearn.neighbors import NearestNeighbors
 from sklearn.cluster import DBSCAN
 
 
-def get_neighbourhood_matrix(D, p, k=3):
+def get_neighbourhood_matrix(D, p, k=200):
     # compute matrix N_p of shape k x d
     nbrs = NearestNeighbors(
         n_neighbors=k, algorithm='brute', metric="euclidean").fit(D)
@@ -13,6 +13,7 @@ def get_neighbourhood_matrix(D, p, k=3):
 
 def get_weak_eigenvectors_matrix(meta):
     pass
+
 
 def covariance(X):
     # covariance matrix of shape dxd
@@ -35,44 +36,41 @@ def covariance_decomposition(X):
 
 
 def total_variance(eigenvalues, r):
-    # calculates the total explained varience of using
+    # calculates the total explained variance of using
     # the first r eigenvalues
-    return sum(eigenvalues[0:r])/sum(eigenvalues)
+    return sum(eigenvalues[0:r]) / sum(eigenvalues)
 
 
-def correlation_dimensionality(D, p, alpha=0.85):
+def correlation_dimensionality(D, p, k, alpha=0.85):
     # finds lambda λ of the given point p
     # which is the smallest number of eigenvalues of the covariance matrix of N_p
     # explaining a portion of at least α of the total variance
 
-    N_p = get_neighbourhood_matrix(D, p)
+    N_p = get_neighbourhood_matrix(D, p, k)
     eigen_values, eigen_vectors = covariance_decomposition(N_p)
     l = 1
     for i in range(D.shape[1]):
-        if total_variance(eigen_values, i) >= alpha:
-            l = i
+        if total_variance(eigen_values, i+1) >= alpha:
+            l = i+1
             break
 
     return l, eigen_values, eigen_vectors
 
 
-def make_partitions(D, k=1000):
-
+def make_partitions(D, k):
     point_info = dict()  # useful for later
     partitions = dict()  # store indices of points per partition
-    E_hats = []          # diagonal weak eigenvector filters
-
+    E_hats = []  # diagonal weak eigenvector filters
 
     # initialize partitions, meta data and eigenvector filter
     for i in range(D.shape[1]):
-        point_info[i+1] = dict()
-        partitions[i+1] = []
+        point_info[i + 1] = dict()
+        partitions[i + 1] = []
         # weak eigenvector filter used to compute 
         # Vq · Êq · VqT
         E_hat = np.eye(D.shape[1])
-        E_hat[0:i+1, 0:i+1] = 0
+        E_hat[0:i + 1, 0:i + 1] = 0
         E_hats.append(E_hat)
-        
 
     # for every point, compute necessary values and store them
     for i, p in enumerate(D):
@@ -80,18 +78,19 @@ def make_partitions(D, k=1000):
         # CHANGE since 2 equal points have the same e and v list, p can be used as key
         # CHANGE encode dimensionality in nested index -> saves memory in dbscan alg.
         # caclulate V * E^ * V.T since this value gets used a lot
-        VEV = v_list @ E_hats[l-1] @ v_list.T 
+        VEV = v_list @ E_hats[l - 1] @ v_list.T
         point_info[l][p.data.tobytes()] = {
             # 'lambda': l,  # integer
             'E': e_list,  # 1D array
             'V': v_list,  # 2D array
-            'VEV': VEV    # 2D array
+            'VEV': VEV  # 2D array
         }
 
         # add index of point to corresponding partition
         partitions[l].append(i)
 
     return point_info, partitions
+
 
 def is_approximate_linear_dependant(V_p, VEV_q, delta_affine):
     deltas = np.empty(V_p.shape[1])
@@ -103,17 +102,21 @@ def is_approximate_linear_dependant(V_p, VEV_q, delta_affine):
     # check sqrt(vi.T · Vq · Êq · Vq.T · vi ≤ Δ) for all vi
     return np.all(deltas < delta_affine)
 
+
 def affine_distance(p, q, VEV_q):
     # compute sqrt((p − q)T · Vq · Êq · Vq.T · (p − q))
-    return np.sqrt((p-q).T @ VEV_q @ (p-q))
+    return np.sqrt((p - q).T @ VEV_q @ (p - q))
+
 
 def symmetric_correlation_distance(
-    x, y, delta_affine, delta_dist,
-    point_info_lx, point_info_ly=None # distances of different dimensionalities will be useful later
+        x, y, delta_affine, delta_dist,
+        l_x, point_info_lx,
+        l_y=None, point_info_ly=None  # distances of different dimensionalities will be useful later
 ):
     # point info dictionaries for dimensionalities lx and ly
     # check if points have same lambda dimensionality
     if point_info_ly is None:
+        l_y = l_x
         point_info_ly = point_info_lx
 
     # get point info data by hashing point
@@ -133,20 +136,21 @@ def symmetric_correlation_distance(
     # ***
     # since the maximum value is chosen 
 
-    if (is_approximate_linear_dependant(V_x, VEV_y, delta_affine) and
+    if (is_approximate_linear_dependant(V_x[:, 0:l_x], VEV_y, delta_affine) and
             affine_distance(x, y, VEV_y) < delta_dist and
-            is_approximate_linear_dependant(V_y, VEV_x, delta_affine) and
+            is_approximate_linear_dependant(V_y[:, 0:l_y], VEV_x, delta_affine) and
             affine_distance(y, x, VEV_x) < delta_dist):
         return 0
     else:
         return 1
 
+
 def cluster_partitions(
-    D, partitions, point_info,
-    delta_affine, delta_dist, min_samples
+        D, partitions, point_info,
+        delta_affine, delta_dist, min_samples
 ):
     # initialize output structures
-    models = [None for _ in partitions] # maybe useful later?
+    models = [None for _ in partitions]  # maybe useful later?
     clusters = {i: [] for i in partitions}
 
     for l, p in partitions.items():
@@ -157,7 +161,8 @@ def cluster_partitions(
             metric_params = {
                 'delta_affine': delta_affine,
                 'delta_dist': delta_dist,
-                'point_info_lx': point_info[l]
+                'point_info_lx': point_info[l],
+                'l_x': l
             }
             # perform DBSCAN
             # eps is the closest value to zero,
@@ -169,20 +174,19 @@ def cluster_partitions(
                 metric_params=metric_params
             ).fit(D[p])
 
-
-        models[l] = model
-        # get indices from model
-        label = 0
-        # iterate labels
-        while True:
-            # get indices of partition where
-            # points are clustered the current label
-            cluster = np.transpose((model.labels_ == label).nonzero())
-            if cluster.size != 0:
-                clusters[l].append(cluster)
-                label += 1
-            else:
-                # stop if model has no more labels
-                break
+            models[l] = model
+            # get indices from model
+            label = 0
+            # iterate labels
+            while True:
+                # get indices of partition where
+                # points are clustered the current label
+                cluster = np.transpose((model.labels_ == label).nonzero())
+                if cluster.size != 0:
+                    clusters[l].append(cluster)
+                    label += 1
+                else:
+                    # stop if model has no more labels
+                    break
 
         return models, clusters
--- ERiC/main.py
+++ ERiC/main.py
@@ -1,21 +1,24 @@
 import numpy as np
+import pandas as pd
 from lib import *
 
 
-
-
-###### MAIN ############
-
-
 # data type important ! float 64 is needed for hashing in dbscan alg.
-D = np.array([[-1, -1], [-2, -1], [-3, -2], [1, 1], [2, 1], [3, 2]], dtype=np.float64)
+#D = np.array([[-1, -1], [-2, -1], [-3, -2], [1, 1], [2, 1], [3, 2]], dtype=np.float64)
 
+df = pd.read_csv("sample_dataset/wages.csv")
+df = df[["AGE", "EDUCATION", "EXPERIENCE", "WAGE"]]
+df.columns = ["A", "YE", "YW", "W"]
+print(df.head())
+D = df.to_numpy(dtype=np.float64)
 
-point_info, partitions = make_partitions(D, 3)
+point_info, partitions = make_partitions(D, k=120)
 print("Info per point:")
-print(point_info)
+#print(point_info)
 print("Partitions:")
 print(*[len(p) for p in partitions.values()])
-models, clusters = cluster_partitions(D, partitions, point_info, 1.5, .5, 2)
 
+models, clusters = cluster_partitions(D, partitions, point_info, 1.5, .5, 2)
+print(models)
+print(clusters)
 
